0c94fe546b3e7086498f4221ca9fb78e262db12a
ning.pan <ning.pan@advantech.com.cn>
2022-07-18 13:29:04 +0800

    Add symbolink for mk-ubuntu.sh in BSP directory

--- mk-ubuntu.sh
+++ mk-ubuntu.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -e
 
 TOP_DIR=`pwd`
+UBUNTU_DIR='ubuntu'
 
 echo Top of tree: ${TOP_DIR}
 
 # make ubuntu base
 if [ "$1" == "new" ]; then
     echo "make ubuntu base new"
+   	cd $TOP_DIR/$UBUNTU_DIR
     RELEASE=focal ARCH=arm64 ./mk-base-ubuntu.sh
 else
     echo "use exist ubuntu base"
-    cat ubuntu-base/ubuntu20.04-*.tar.gz* > ubuntu20.04-whole.tar.gz
+    cat $UBUNTU_DIR/ubuntu-base/ubuntu20.04-*.tar.gz* > $UBUNTU_DIR/ubuntu20.04-whole.tar.gz
 fi
 
 # mk rockchip
+cd $TOP_DIR/$UBUNTU_DIR
 ARCH=arm64 ./mk-rootfs-focal.sh
 
 # mk advantech
