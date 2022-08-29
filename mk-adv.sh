a96b33e639566122849f637050e4c5ff1daa3411
ning.pan <ning.pan@advantech.com.cn>
2022-08-29 13:53:56 +0800

    Add cpufrequtils

--- mk-adv.sh
+++ mk-adv.sh
@@ -82,6 +82,8 @@ apt-get install -y libqt5quickwidgets5
 apt-get install -y libqt5webenginewidgets5
 # udisks2
 apt-get install -y libblockdev-mdraid2
+# cpufreq-info
+apt-get install -y cpufrequtils
 #---------------Adjust--------------
 systemctl enable advinit.service
 systemctl enable adv-wifi-init.service
